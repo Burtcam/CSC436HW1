84dea911c6ddd3368b768bce73c4a1306d1be9dd
Cam <Silkenmeerkat@users.noreply.github.com>
2020-04-16 18:57:52 -0500

    Pushing with comments that highlight problem
    Problem highlighted

--- relation2.py
+++ relation2.py
@@ -44,6 +44,62 @@ def relationread():
 
     return relation, funcdep
 
+#todo
+'''
+# don't touch... not sure if needed but we'll talk about it when the rest of the group looks at it?
+def checkwhatcanbemade(key, funcDepend):
+    #loop through key, call each on funcDepend, if found, add to key and call the value on funcDepend until an element in Key already is found
+    for i in range(len(key)):
+        #Todo bug fix here. This will throw an error in the event of a compound key being provided.
+        #if compound key, check if both elements are in the key
+        if len(key[i] >1):
+            flag = 1
+            for j in range(len(key[i])):
+                if key[j] not in key:
+                    flag = 0
+                    break
+        ##if all elements of compound key in keylist then we can find all of the values of the compound key
+       # if flag == 1:
+
+        key.append(funcDepend[key[i]])
+        temp = funcDepend[key[i]]
+        ##KEEP GOING UNTIL result of funcDepend[key[i] is not in funcDepend.values()
+        while temp in funcDepend:
+            temp = funcDepend[temp]
+            key.append(temp)
+
+    return key
+
+
+#call each element of key list on dict. Add the result to found list O(n) for loop, O(1) for each dict call
+# #found = checkwhatcanbemade(copy.deepcopy(key), funcDepend)
+#TODO loop through funcDepend keys and check that using those keys, you can find ALL members of relation. If not, add what is needed to find that
+def checkrevised(keylist, funcDepend):
+
+    return 0
+    ##loop through funcDepend values,
+    
+'''
+
+
+#check which elements of relation are not on right side of dict O(n) put them in Key list (values) of dict
+def onlyleftsidecrap(funcDepend, relation):
+    listToExclude = set()
+
+    for value in funcDepend.values():
+        if len(value) > 1:
+            for j in range(len(value)):
+                listToExclude.add(value[j])
+        else:
+            listToExclude.add(value)
+
+    keyList = []
+
+    for i in range(len(relation)):
+        if relation[i] not in listToExclude:
+            keyList.append(relation[i])
+
+    return keyList
 
 
 def leftside(funcDepend, relation):
@@ -117,6 +173,7 @@ def neitherRightOrLeft(relation,leftNoDupes,rightNoDupes):
 
 #find all elements NOT in combined and right side
 def exteriors(combined, right, relation):
+    #print(combined)
 
     ext =[]
     for i in range(len(relation)):
@@ -161,7 +218,6 @@ def closuretest(totest,funcDepends, relation):
 
     #remove dupes
     totest = "".join(set(totest))
-    #print("totest at the end", totest)
     if len(totest) == len(relation):
         #print("True")
         return True
@@ -169,7 +225,7 @@ def closuretest(totest,funcDepends, relation):
         #print("false")
         return False
 
-    #TODO all elements of new must be in every permutation, however there can be anywhere between 1 and n elements of extern in every permutation.
+
 # Add exterior values to complete keys
 def combineExteriors(keyList, exter, funcDepend, relation):
     if (len(exter) != 0):
@@ -182,6 +238,7 @@ def combineExteriors(keyList, exter, funcDepend, relation):
         finalKeys = keyList
 
     return finalKeys
+
 
 def main():
     msgbox('Please choose the file which holds the relation and functional dependencies')
@@ -208,24 +265,36 @@ def main():
     neither =neitherRightOrLeft(relation,leftList,rightList)
     #print ("neither =" ,neither)
     keyList = []
+    flag = 0
 
-    new = leftNoDupes+neither
     new = (''.join(map(str, leftNoDupes + neither)))
     #test if combined is a closure if it is, print it and end it all right here!
-    if ((closuretest(new, funcDepend, relation)) ==True):
+    #print("testing")
+    if (closuretest(new, funcDepend, relation)):
         keyList.append(new)
-        print("The candidate key is = ", keyList)
-        exit(1)
+        print("The Key's are: ", keyList)
+
+    #TODO PROBLEM FOUND, The issue is that it comes up true always and then goes to print both times.
+    exter = exteriors(neither+leftNoDupes,rightNoDupes, relation)
+    keyList = combineExteriors(keyList, exter, funcDepend, relation)
+    print("The Keys are: ", keyList)
+
 
 
 
     #combine the elements of neither and leftnoDupes then find the closures using the relational dependencies
-    exter = exteriors(neither+leftNoDupes,rightNoDupes, relation)
+    #exter = exteriors(neither+leftNoDupes,rightNoDupes, relation)
     #print ("exteriors = " , exter)
 
 
     #TODO deal with the problem of how to create a list of every permutation of "new" + the externs list then test each of them against closuretest
-    keyList = combineExteriors(keyList, exter, funcDepend, relation)
 
-    print("The options for candidate keys are: ", keyList)
+    # If there are values in extern, combine with keys.
+
+    #keyList = combineExteriors(keyList, exter, funcDepend, relation)
+
+    #print("The Keys are: ", keyList)
+   # exit(0)
+
+
 main()
